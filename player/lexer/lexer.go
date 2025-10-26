e016c5ce303326c28260acc2e71e983117ba4ce9
Zac Delventhal <contrib@zacis.me>
2025-10-26 14:38:49 -0500

    Add initial text lexing
    Text flows until the end of the block and is then trimmed. Since I
    haven't implemented any of the block delimiters yet, it just flows to
    the end of the file for now.

--- player/lexer/lexer.go
+++ player/lexer/lexer.go
@@ -1,14 +1,41 @@
 package lexer
 
-import "tale/tokens"
+import (
+	"strings"
+	"tale/tokens"
+	"unicode/utf8"
+)
 
 type Lexer struct {
 	input string
 	pos int
+	nextPos int
+	current rune
+}
+
+func (l *Lexer) read() {
+	r, w := utf8.DecodeRuneInString(l.input[l.pos:])
+	l.current = r
+	l.nextPos = l.pos + w
+}
+
+func (l *Lexer) nextBlockStart() int {
+	return len(l.input)
+}
+
+func (l *Lexer) captureText() string {
+	end := l.nextBlockStart()
+	text := l.input[l.pos:end]
+	l.pos = end
+	l.read()
+
+	return strings.TrimSpace(text)
 }
 
 func New(input string) *Lexer {
-	return &Lexer{input, 0}
+	l := &Lexer{input: input}
+	l.read()
+	return l
 }
 
 func (l *Lexer) Next() tokens.Token {
@@ -16,5 +43,6 @@ func (l *Lexer) Next() tokens.Token {
 		return tokens.Token{tokens.EOF, ""}
 	}
 
-	return tokens.Token{tokens.ILLEGAL, string(l.input[l.pos])}
+	text := l.captureText()
+	return tokens.Token{tokens.TEXT, text}
 }
--- player/lexer/lexer_test.go
+++ player/lexer/lexer_test.go
@@ -5,12 +5,40 @@ import (
     "testing"
 )
 
-func TestEof(t *testing.T) {
-	lex := New("")
-	actual := lex.Next()
-	expected := tokens.Token{tokens.EOF, ""}
+func expectTokens(t *testing.T, input string, expected []tokens.Token) {
+    lex := New(input)
 
-	if actual != expected {
-		t.Fatalf("Eof Failed! expected=%v, got=%v", expected, actual)
-	}
+    for i, exp := range expected {
+        act := lex.Next()
+        if act != exp {
+            t.Fatalf("[%d] expected=%v, got=%v", i, exp, act)
+        }
+    }
+}
+
+func TestEof(t *testing.T) {
+    expectTokens(t, "", []tokens.Token{
+        {tokens.EOF, ""},
+    })
+}
+
+func TestText(t *testing.T) {
+    expectTokens(t, "Hello, world!", []tokens.Token{
+        {tokens.TEXT, "Hello, world!"},
+        {tokens.EOF, ""},
+    })
+}
+
+func TestUnicode(t *testing.T) {
+    expectTokens(t, "Hello, 世界!", []tokens.Token{
+        {tokens.TEXT, "Hello, 世界!"},
+        {tokens.EOF, ""},
+    })
+}
+
+func TestTrim(t *testing.T) {
+    expectTokens(t, "\n\n    Hello, \nworld!\n\t", []tokens.Token{
+        {tokens.TEXT, "Hello, \nworld!"},
+        {tokens.EOF, ""},
+    })
 }
